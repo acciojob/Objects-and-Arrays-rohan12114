2ae2bc99bae8890ae312d9155bb180419eb74898
rohan12114 <122142354+rohan12114@users.noreply.github.com>
2023-05-20 18:43:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-const players = ["John", "Bob", "Alice", "Poppy"];
+const players = ['John', 'Bob', 'Alice', 'Poppy'];
 
 const person = {
-  name: "John Doe",
-  age: 80,
-};
+      name: 'John Doe',
+      age: 80
+    };
+const team = players;
+const team1 = players.slice();
 
-// Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
+//let copiedPerson = Object.assign({}, person);
+let cap1 = Object.assign({}, person);
 
 window.players = players;
 window.person = person;
 window.team = team;
 window.team1 = team1;
-window.cap1 = cap1;
